f9302a2c3f1c2a1749290f443c6249ebc119cd93
Andrey Sankovich <53842066+bANSHEE-dev@users.noreply.github.com>
2020-03-13 19:17:16 +0300

    feat: fix solution

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 module.exports = function towelSort (matrix) {
-  if(!matrix) return [];
   let newArr = [];
-  
-  for (let i = 0; i < matrix.length; i++) {
-    if((i+1) % 2 === 0){
-      for (let y = matrix[i].length - 1; j >= 0 ; j--) {
-        newArr.push(matrix[i][j])
-      }
-    } else{
-      for (let j = 0; j < matrix[i].length; j++) {
-        newArr.push(matrix[i][j])
-      }
-    } 
+  if (matrix) {
+      matrix.forEach((item, j) => {
+          if (j % 2) {
+              item.reverse();
+          }
+          newArr = newArr.concat(item);
+      });
   }
   return newArr;
 }
